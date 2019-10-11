2c4c3c723701f365abed0b1b7b045a6c7d0f2484
Michael Duberstein <mduberst@amazon.com>
2019-10-11 09:28:40 -0400

    After Clip 352  Rules of Reducers: call to SelectionReducer logged action to prove it is called

--- src/reducers/SelectionReducer.js
+++ src/reducers/SelectionReducer.js
@@ -1,4 +1,14 @@
-export default (state, action) => {
-  console.log(action);
-  return null;
+// default parameter takes care of the first reducer call at startup, when initial state === undefined
+export default (state = null, action) => {
+  // console.log(action);
+  // // initial state return acceptable to redux
+  // return null;
+
+  // reducer boilerplate
+  switch(action.type){
+    case 'select_library':
+      return action.payload;
+    default:
+      return state;
+  }
 };
